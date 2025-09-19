aa39564857c9dd4c58868b3abad6e5c45d78c40e
Faris <faris.223040126@mail.unpas.ac.id>
2025-09-19 07:38:13 +0700

    refactor(server): rename index.js to server.js

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const sequelize = require("./config/database");
+const allRoutes = require("./routes/index");
+const About = require("./models/About");
+const GalleryImage = require("./models/GalleryImage");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", allRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
+
+// Fungsi untuk menjalankan server
+async function startServer() {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log("✅ Semua model telah disinkronisasi.");
+    app.listen(port, () => {
+      console.log(`🚀 Server berjalan di http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("❌ Gagal menjalankan server:", error);
+  }
+}
+
+startServer();
