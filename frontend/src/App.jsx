af4f138fcd4018e1f546dcd9bff5a849b09f0e63
Faris <faris.223040126@mail.unpas.ac.id>
2025-09-24 14:02:20 +0700

    refactor(frontend): Rapihkan struktur route di App.jsx dan hilangkan duplikat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./admin/components/MainLayout";
 
-// Halaman Produk
-import ProductPage from "./admin/pages/products/ProductPage";
-import ProductFormPage from "./admin/pages/products/AddProductPage";
-
-// Halaman About
-import AboutPage from "./admin/pages/about/AboutPage";
-
-// Halaman Gallery
-import GalleryPage from "./admin/pages/gallery/GalleryPage";
-import GalleryFormPage from "./admin/pages/gallery/GalleryFormPage";
-
-// Halaman Ingredients
-import IngredientPage from "./admin/pages/ingredients/IngredientPage";
-import AddIngredientPage from "./admin/pages/ingredients/AddIngredientPage";
-
-// Halaman Lab Tools
-import LabToolPage from "./admin/pages/lab_tools/LabToolPage";
-import AddLabToolPage from "./admin/pages/lab_tools/AddLabToolPage";
-
-// Halaman Homepage
+// Homepage
 import Homepage from "./admin/pages/homepage/Homepage";
 import AddLandingHeadingPage from "./admin/pages/homepage/AddLandingHeadingPage";
 
-// Halaman Articles
+// Articles
 import ArticlePage from "./admin/pages/articles/ArticlePage";
 import AddArticlePage from "./admin/pages/articles/AddArticlePage";
+
+// Products
+import ProductPage from "./admin/pages/products/ProductPage";
+import ProductFormPage from "./admin/pages/products/AddProductPage";
+
+// Ingredients
+import IngredientPage from "./admin/pages/ingredients/IngredientPage";
+import AddIngredientPage from "./admin/pages/ingredients/AddIngredientPage";
+
+// Lab Tools
+import LabToolPage from "./admin/pages/lab_tools/LabToolPage";
+import AddLabToolPage from "./admin/pages/lab_tools/AddLabToolPage";
+
+// Gallery
+import GalleryPage from "./admin/pages/gallery/GalleryPage";
+import GalleryFormPage from "./admin/pages/gallery/GalleryFormPage";
+
+// About
+import AboutPage from "./admin/pages/about/AboutPage";
 
 const PlaceholderPage = ({ title }) => <h1 className="text-4xl font-bold text-white">{title}</h1>;
 
@@ -35,28 +35,42 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
+          {/* Homepage */}
           <Route index element={<Homepage />} />
           <Route path="homepage" element={<Homepage />} />
           <Route path="homepage/add" element={<AddLandingHeadingPage />} />
           <Route path="homepage/edit/:id" element={<AddLandingHeadingPage />} />
+
+          {/* Articles */}
           <Route path="articles" element={<ArticlePage />} />
           <Route path="articles/add" element={<AddArticlePage />} />
           <Route path="articles/edit/:id" element={<AddArticlePage />} />
-          <Route path="workshop" element={<PlaceholderPage title="Workshop" />} />
+
+          {/* Catalogue - Products */}
           <Route path="catalogue/products" element={<ProductPage />} />
           <Route path="catalogue/products/add" element={<ProductFormPage />} />
           <Route path="catalogue/products/edit/:id" element={<ProductFormPage />} />
+
+          {/* Catalogue - Ingredients */}
           <Route path="catalogue/ingredients" element={<IngredientPage />} />
           <Route path="catalogue/ingredients/add" element={<AddIngredientPage />} />
           <Route path="catalogue/ingredients/edit/:id" element={<AddIngredientPage />} />
+
+          {/* Catalogue - Lab Tools */}
           <Route path="catalogue/lab-tools" element={<LabToolPage />} />
           <Route path="catalogue/lab-tools/add" element={<AddLabToolPage />} />
-          <Route path="catalogue/lab-tools/edit/:id" element={<AddLabToolPage />} /> {/* <-- Route yang hilang ditambahkan */}
-          <Route path="about" element={<AboutPage />} />
-          <Route path="articles" element={<PlaceholderPage title="Articles" />} />
+          <Route path="catalogue/lab-tools/edit/:id" element={<AddLabToolPage />} />
+
+          {/* Gallery */}
           <Route path="gallery" element={<GalleryPage />} />
           <Route path="gallery/add" element={<GalleryFormPage />} />
           <Route path="gallery/edit/:id" element={<GalleryFormPage />} />
+
+          {/* About */}
+          <Route path="about" element={<AboutPage />} />
+
+            {/* workshop */}
+          <Route path="workshop" element={<PlaceholderPage title="Workshop" />} />
         </Route>
       </Routes>
     </Router>
