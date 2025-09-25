38b8ed27f1b5a3db5b38e3baed96cd5e17775077
Faris <faris.223040126@mail.unpas.ac.id>
2025-09-26 04:01:12 +0700

    feat(api): Add Workshop model and many-to-many relation with Events

---
+++ backend/models/Workshop.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Workshop = sequelize.define("Workshop", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM("draft", "published"),
+    defaultValue: "draft",
+    allowNull: false,
+  },
+});
+
+module.exports = Workshop;
