938d76e68bbe1dc4c6c16cb8d8b01b5f2e850c32
Faris <faris.223040126@mail.unpas.ac.id>
2025-10-11 23:02:21 +0700

    feat(translate): Install i18next dan setup konfigurasi dasar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
-// frontend/src/main.jsx
-
-import React from "react";
+import React, { Suspense } from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Suspense>
   </React.StrictMode>
 );
