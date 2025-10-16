63159533ba66c05cc7b9715a4bc51efc112a9b9d
Faris <faris.223040126@mail.unpas.ac.id>
2025-10-16 22:24:59 +0700

    feat(i18n): Setup awal fitur translasi (i18next)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 // frontend/src/main.jsx
-
-import React from "react";
+import React, { Suspense } from "react"; // Tambahkan Suspense
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import "./i18n"; // Impor konfigurasi i18n
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+  ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Suspense>
   </React.StrictMode>
 );
